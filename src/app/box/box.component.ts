72d26e321899e5b56a84f3cd319356a59755ece2
juicetinliu <37272242+juicetinliu@users.noreply.github.com>
2020-08-04 21:51:26 +0800

    Small update

--- src/app/box/box.component.ts
+++ src/app/box/box.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from '../models/Project';
 
 @Component({
   selector: 'app-box',
@@ -6,10 +7,74 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./box.component.css']
 })
 export class BoxComponent implements OnInit {
+  projects: Project[];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.projects = [
+      {
+        title: 'Project 1',
+        description: 'This is a description for project 1. Put the short description here',
+        url: 'string',
+        image: 'string',
+      },
+      {
+        title: 'Project 2',
+        description: 'This is a description for project 2. Put the short description here',
+        url: 'string',
+        image: 'string',
+      },
+      {
+        title: 'Project 3',
+        description: 'This is a description for project 3. Put the short description here',
+        url: 'string',
+        image: 'string',
+      },
+      {
+        title: 'Project 4',
+        description: 'This is a description for project 4. Put the short description here',
+        url: 'string',
+        image: 'string',
+      },
+      {
+        title: 'Project 5',
+        description: 'This is a description for project 5. Put the short description here',
+        url: 'string',
+        image: 'string',
+      },
+      {
+        title: 'Project 6',
+        description: 'This is a description for project 6. Put the short description here',
+        url: 'string',
+        image: 'string',
+      },
+      {
+        title: 'Project 7',
+        description: 'This is a description for project 7. Put the short description here',
+        url: 'string',
+        image: 'string',
+      },
+      {
+        title: 'Project 8',
+        description: 'This is a description for project 8. Put the short description here',
+        url: 'string',
+        image: 'string',
+      },
+      {
+        title: 'Project 9',
+        description: 'This is a description for project 9. Put the short description here',
+        url: 'string',
+        image: 'string',
+      },
+      {
+        title: 'Project 10',
+        description: 'This is a description for project 10. Put the short description here',
+        url: 'string',
+        image: 'string',
+      },
+
+    ]
   }
 
 }
---
+++ src/app/models/Project.ts
@@ -0,0 +1,6 @@
+export class Project {
+  title: string;
+  description: string;
+  url: string;
+  image: string;
+}
